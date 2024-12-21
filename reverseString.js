63946d8601b8577aaf16b63786bbf8b68da1c6ee
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-12-21 14:13:45 +0200

    add extra checks to function

--- reverseString.js
+++ reverseString.js
@@ -1,4 +1,7 @@
 function reverseString(str) {
+    if (typeof str !== 'string') return 'Please enter a string';
+    if (str.trim() === '') return 'Your string should not be empty';
+    
     return str.split('').reverse().join('');
 }
 
