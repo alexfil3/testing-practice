1f5688e25fd40babf95957f6887521dde9ee81d5
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-12-21 14:10:44 +0200

    add additional checks

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,7 @@
 function capitalize(str) {
+    if (typeof str !== 'string') return 'Please enter a string';
+    if (str.trim() === '') return 'Your string should not be empty';
+
     return str[0].toUpperCase() + str.slice(1);
 }
 
